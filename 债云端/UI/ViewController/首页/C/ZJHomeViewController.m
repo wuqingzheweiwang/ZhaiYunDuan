459bcb87ad59574c183149b5f12678a31998a5af
apple <474920587@qq.com>
2017-07-18 11:00:43 +0800

    1

--- 债云端/UI/ViewController/首页/C/ZJHomeViewController.m
+++ 债云端/UI/ViewController/首页/C/ZJHomeViewController.m
@@ -59,7 +59,6 @@ static NSString *identifierId=@"zz";
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     self.imageArray = [NSMutableArray array];
     self.silderArray= [NSMutableArray array];
     // 设置导航栏
@@ -554,18 +553,18 @@ static NSString *identifierId=@"zz";
         self.tabBarController.selectedIndex=3;
         
     }else if (indexPath.row==9){  //全部应用
-//        if (isOpen) {
-//            ZJAllApplyViewController *addDebtVC = [[ZJAllApplyViewController alloc]init];
-//            [addDebtVC setHidesBottomBarWhenPushed:YES];
-//            [self.navigationController pushViewController:addDebtVC animated:YES];
-//        }else{
-//            [ZJUtil showBottomToastWithMsg:@"该功能正在开发中，敬请期待"];
-//           
-//        }
-        ZJPayMoneyViewController * payVC=[[ZJPayMoneyViewController alloc]initWithNibName:@"ZJPayMoneyViewController" bundle:nil];
-        payVC.orderid=@"145922470223";
-        [payVC setHidesBottomBarWhenPushed:YES];
-        [self.navigationController pushViewController:payVC animated:YES];
+        if (isOpen) {
+            ZJAllApplyViewController *addDebtVC = [[ZJAllApplyViewController alloc]init];
+            [addDebtVC setHidesBottomBarWhenPushed:YES];
+            [self.navigationController pushViewController:addDebtVC animated:YES];
+        }else{
+            [ZJUtil showBottomToastWithMsg:@"该功能正在开发中，敬请期待"];
+           
+        }
+//        ZJPayMoneyViewController * payVC=[[ZJPayMoneyViewController alloc]initWithNibName:@"ZJPayMoneyViewController" bundle:nil];
+//        payVC.orderid=@"145922470223";
+//        [payVC setHidesBottomBarWhenPushed:YES];
+//        [self.navigationController pushViewController:payVC animated:YES];
     }
     
 }
