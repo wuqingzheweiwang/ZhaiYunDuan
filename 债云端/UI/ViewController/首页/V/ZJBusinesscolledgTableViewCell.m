5c5274afca0fab2a55677c54095e2d3e07ef4a8d
apple <474920587@qq.com>
2017-07-19 21:11:28 +0800

    商学院布局优化

--- 债云端/UI/ViewController/首页/V/ZJBusinesscolledgTableViewCell.m
+++ 债云端/UI/ViewController/首页/V/ZJBusinesscolledgTableViewCell.m
@@ -28,39 +28,47 @@
 -(void)setitem:(ZJHomeNewsModel *)item
 {
 
-    backImage.top=TRUE_1(15);
+    backImage.top=TRUE_1(20);
     backImage.left=TRUE_1(15);
     backImage.width=TRUE_1(250/2);
     backImage.height=TRUE_1(70);
     [backImage sd_setImageWithURL:[NSURL URLWithString:item.img] placeholderImage:[UIImage imageNamed:@"backGroundDefault"]];
 
-    titileText.top = TRUE_1(10);
-    titileText.left = backImage.right+TRUE_1(15);
-    titileText.width = ZJAPPWidth - titileText.left -backImage.left;
-    titileText.numberOfLines =2;
-    titileText.height = backImage.height/3;
-    titileText.font = ZJ_TRUE_FONT(14);
-    titileText.text = item.title;
     
-    detialText.top = titileText.bottom+TRUE_1(7);
+    detialText.top = titileText.bottom;
     detialText.left = titileText.left;
     detialText.width = titileText.width;
-    detialText.height = backImage.height/3;
+    detialText.height = TRUE_1(25);
     detialText.numberOfLines =2;
     detialText.text = item.title;
-    detialText.font = ZJ_TRUE_FONT(9);
+    detialText.font = ZJ_TRUE_FONT(10);
     
-    timeText.top = detialText.bottom+TRUE_1(5);
-    timeText.left = detialText.left;
-    timeText.width = detialText.width;
-    timeText.height = backImage.height/3;
-    timeText.font = ZJ_TRUE_FONT(9);
+    titileText.top = TRUE_1(20);
+    titileText.left = backImage.right+TRUE_1(15);
+    titileText.width = ZJAPPWidth - TRUE_1(45) -backImage.width;
+    titileText.font=ZJ_TRUE_FONT(14);
+    titileText.text=item.title;
+    titileText.height = TRUE_1(20);
+    
+    detialText.top = TRUE_1(40);
+    detialText.left = titileText.left;
+    detialText.width = titileText.width;
+    detialText.font=ZJ_TRUE_FONT(10);
+    detialText.numberOfLines = 0;
+    detialText.text=item.title;
+    detialText.height = TRUE_1(30);
+    
+    timeText.top = TRUE_1(80);
+    timeText.left = titileText.left;
+    timeText.width = titileText.width;
+    timeText.height = TRUE_1(10);
+    timeText.font = ZJ_TRUE_FONT(10);
     timeText.text = item.updateTime;
 }
 
 + (CGFloat)getCellHeight
 {
-    return 100;
+    return TRUE_1(90);
 }
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
