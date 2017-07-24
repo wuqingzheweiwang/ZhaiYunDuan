23d8546fd05b9ef5d5ee70efc53c959356f30ff0
apple <474920587@qq.com>
2017-07-24 18:54:54 +0800

    1

--- 债云端/UI/ZJRequestData/ZJDataRequest.m
+++ 债云端/UI/ZJRequestData/ZJDataRequest.m
@@ -194,8 +194,9 @@
 #pragma 拼接http
 - (NSString *)requestHttpURL:(NSString *)action
 {
-    return [NSString stringWithFormat:@"http://test.api.zhongjinzhaishi.com/%@",action];
+//    return [NSString stringWithFormat:@"http://test.api.zhongjinzhaishi.com/%@",action];
 //    return [NSString stringWithFormat:@"http://192.168.2.11:8056/%@",action];
+    return [NSString stringWithFormat:@"http://api.zhongjinzhaishi.com/%@",action];
 }
 
 
