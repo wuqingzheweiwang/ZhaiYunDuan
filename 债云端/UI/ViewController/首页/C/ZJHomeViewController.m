7342ea111c1becbcd5f38c8a6baefa5474d2f665
apple <474920587@qq.com>
2017-08-02 19:08:01 +0800

    推荐备案  角色

--- 债云端/UI/ViewController/首页/C/ZJHomeViewController.m
+++ 债云端/UI/ViewController/首页/C/ZJHomeViewController.m
@@ -101,6 +101,10 @@ static NSString *identifierId=@"zz";
              if ([[responseData objectForKey:@"state"]isEqualToString:@"ok"]) {
                  [ZJUserInfo saveUserInfoWithUserRole:[NSString stringWithFormat:@"%@",[[responseData objectForKey:@"data"] objectForKey:@"userType"]]];
                  [ZJUserInfo saveUserInfoWithUserPhone:[NSString stringWithFormat:@"%@",[[responseData objectForKey:@"data"] objectForKey:@"phoneNumber"]]];
+                 if ([[responseData objectForKey:@"data"] objectForKey:@"hangtype"]) {
+                     [ZJUserInfo saveUserInfoWithUserhangtype:[NSString stringWithFormat:@"%@",[[responseData objectForKey:@"data"] objectForKey:@"hangtype"]]];
+                 }
+                 
              }
          }
      }];
